c4b6616a23fdcdb40a499ba6b68ed8d8c4c95b39
Steve Tooke <steve@took.es>
2024-10-25 17:57:26 +0100

    Metrics

--- src/metrics.py
+++ src/metrics.py
@@ -15,6 +15,10 @@ from fetch import fetch_paginated_data
 
 app = typer.Typer(no_args_is_help=True)
 
+parse_app = typer.Typer()
+app.add_typer(parse_app, name="parse")
+
+
 BECAME_NON_COMPLIANT = 'became-non-compliant'
 BECAME_COMPLIANT = 'became-compliant'
 STAYED_NON_COMPLIANT = 'stayed-non-compliant'
@@ -43,59 +47,60 @@ def save_to_json(data: List[Dict[str, Any]], output_file: str) -> None:
         json.dump({'data': data}, f, indent=2)
 
 
-# def get_last_change(group):
-#     mask = ~group['event'].isin([BECAME_COMPLIANT, BECAME_NON_COMPLIANT])
-#     group['last_change'] = group['created_at']
-#     group.loc[mask, 'last_change'] = group['last_change'].shift(1)
-#     return group
-
-
-# def get_last_change(group):
-#     mask = ~group['event'].isin([BECAME_COMPLIANT, BECAME_NON_COMPLIANT])
-#     last_change = group['created_at'].copy()
-#     last_change[mask] = last_change.shift(1)
-#     return last_change
-
-def get_last_change(group):
-    # group['created_at'].where(group['event'] in [BECAME_COMPLIANT, BECAME_NON_COMPLIANT], group['created_at'], None)
-    return np.where(group['event'].isin([BECAME_NON_COMPLIANT, BECAME_COMPLIANT]), group['created_at'], None)
-
-
-@app.command()
-def analyze(
-        data_file: str = typer.Option()
+@parse_app.command()
+def flow(
+        org: Annotated[str, typer.Option()],
+        flow: Annotated[str, typer.Option()],
+        from_ts: int = None,
+        to_ts: int = None,
+        api_key: Annotated[str, typer.Option(envvar="KOSLI_API_KEY")] = None,
+        kosli_host: Annotated[str, typer.Option(envvar="KOSLI_HOST")] = "https://app.kosli.com",
+        out: str = None
 ) -> None:
-    data = json.load(open(data_file))['data']
+    with Progress() as progress:
+        data = fetch_paginated_data(
+            base_url(flow, kosli_host, org),
+            params={'from_timestamp': from_ts, 'to_timestamp': to_ts},
+            api_key=api_key,
+            progress=progress
+        )
 
-    df = pd.DataFrame(data)
-    df['created_at'] = pd.to_datetime(df['created_at'], unit='s')
-    df['date'] = df['created_at'].dt.date
-    df['time'] = df['created_at'].dt.time
-
-    df['nar_id'] = df['annotations'].apply(lambda x: x.get('NAR_ID'))
-    df['repository'] = df['annotations'].apply(lambda x: x.get('REPOSITORY'))
-    df['branch'] = df['git_commit_info'].apply(lambda x: x.get('branch'))
-
-    df['prev_is_compliant'] = df.groupby(['attestation_name', 'branch'])['is_compliant'].shift(1)
-    df['event'] = df.apply(lambda x : LOOKUP[(x['prev_is_compliant'], x['is_compliant'])], axis=1)
-
-
-    table = Table(show_header=True, header_style="bold")
-
-    # Add columns
-    table.add_column("NAR ID")
-    table.add_column("Repository")
-    table.add_column("Branch")
-    table.add_column("Date")
-    table.add_column("Time")
-    table.add_column("Compliant", justify="center")
-    table.add_column("Event")
-
-    # Add rows
-    for _, row in df[[
+    df = create_data_frame(data)
+    columns = [
         "nar_id", "repository", "branch", "date",
         "time", "is_compliant", "event"
-    ]].iterrows():
+    ]
+    render_data_frame(df, columns, out)
+
+
+@parse_app.command()
+def file(
+        data_file: str,
+        out: str = None
+) -> None:
+    data = json.load(open(data_file))['data']
+    df = create_data_frame(data)
+    columns = [
+        "nar_id", "repository", "branch", "date",
+        "time", "is_compliant", "event"
+    ]
+    render_data_frame(df, columns, out)
+
+
+def render_data_frame(df, columns, out):
+    if out is None:
+        print_table(df, columns)
+    else:
+        df[columns].to_csv(out, index=False)
+
+
+def print_table(df, columns):
+    table = Table(show_header=True, header_style="bold")
+    # Add columns
+    for column in columns:
+        table.add_column(column)
+    # Add rows
+    for _, row in df[columns].iterrows():
         table.add_row(
             str(row["nar_id"]),
             str(row["repository"]),
@@ -105,8 +110,20 @@ def analyze(
             str(row["is_compliant"]),
             str(row["event"]),
         )
-
     console.print(table)
+
+
+def create_data_frame(data):
+    df = pd.DataFrame(data)
+    df['created_at'] = pd.to_datetime(df['created_at'], unit='s')
+    df['date'] = df['created_at'].dt.date
+    df['time'] = df['created_at'].dt.time
+    df['nar_id'] = df['annotations'].apply(lambda x: x.get('NAR_ID'))
+    df['repository'] = df['annotations'].apply(lambda x: x.get('REPOSITORY'))
+    df['branch'] = df['git_commit_info'].apply(lambda x: x.get('branch'))
+    df['prev_is_compliant'] = df.groupby(['attestation_name', 'branch'])['is_compliant'].shift(1)
+    df['event'] = df.apply(lambda x: LOOKUP[(x['prev_is_compliant'], x['is_compliant'])], axis=1)
+    return df
 
 
 @app.command()
@@ -143,8 +160,7 @@ def download(
 
 
 def base_url(flow, kosli_host, org):
-    base_url = f"{kosli_host}/api/v2/attestations/{org}/{flow}"
-    return base_url
+    return f"{kosli_host}/api/v2/attestations/{org}/{flow}"
 
 
 if __name__ == "__main__":
